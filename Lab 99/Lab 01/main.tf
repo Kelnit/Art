f05f5d1fc43225d9aa9ff176e71959a8d9692b95
Kelnit <kelpintech@gmail.com>
2026-01-18 13:32:08 +0700

    Hi ! 99 : 01

--- Lab 99/Lab 01/main.tf
+++ Lab 99/Lab 01/main.tf
@@ -1,16 +1,27 @@
+# Build Infra with Terraform on Google Cloud : Terraform Fundamentals
+
+# Building Google Compute Engine
+# Default : Block Identifier for the Corresponding Resources
 resource "google_compute_instance" "default" {
   name         = "terraform"
   project      = var.project
   machine_type = var.machine_type
   zone         = var.zone
 
+  # the Hard Drive of this Compute Engine
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-12"
     }
   }
 
+  # Network Config to this Compute Engine
   network_interface {
     network = "default"
+
+    # Public IP Address Only
+    access_config {
+      # Ephemeral Public IP
+    }
   }
 }
